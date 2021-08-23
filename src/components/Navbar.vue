8c612d7e7eb2273d174933f6a1c6c70a2875bba3
sssulai@gmail.com <you@example.com>
2021-08-23 15:36:31 -0400

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <v-toolbar app color="#022770">
+      <v-toolbar-title class="uppercase">
+        <span class="#fff">Sulai Sivadel </span>
+        <span>Web Developer</span>
+      </v-toolbar-title>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,94 @@
 <template>
-  <hello-world />
+  <v-sheet
+    color="#4680F2"
+    elevation="24"
+    height="1200"
+    outlined
+    rounded
+    shaped
+    width="1000"
+  >
+    <div class="headline">
+      <v-card max-width="1000" class="mx-auto">
+        <v-system-bar color="pink darken-2" dark> </v-system-bar>
+        <v-spacer></v-spacer>
+        <v-container>
+          <v-row dense>
+            <v-col cols="12">
+              <v-card color="#385F73" dark>
+                <v-card-title class="text-h5"> </v-card-title>
+
+                <v-card-subtitle
+                  >Listen to your favorite artists and albums whenever and
+                  wherever, online and offline.</v-card-subtitle
+                >
+
+                <v-card-actions>
+                  <v-btn text> Listen Now </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+
+            <v-col v-for="(item, i) in items" :key="i" cols="12">
+              <v-card :color="item.color" dark>
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <div>
+                    <v-card-title
+                      class="text-h5"
+                      v-text="item.title"
+                    ></v-card-title>
+
+                    <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                    <v-card-actions>
+                      <v-btn
+                        v-if="item.artist === 'Ellie Goulding'"
+                        class="ml-2 mt-3"
+                        fab
+                        icon
+                        height="40px"
+                        right
+                        width="40px"
+                      >
+                        <v-icon>mdi-play</v-icon>
+                      </v-btn>
+
+                      <v-btn v-else class="ml-2 mt-5" outlined rounded small>
+                        START RADIO
+                      </v-btn>
+                    </v-card-actions>
+                  </div>
+
+                  <v-avatar class="ma-3" size="125" tile>
+                    <v-img :src="item.src"></v-img>
+                  </v-avatar>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </div>
+  </v-sheet>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+    items: [
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#952175",
+        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+        title: "Halcyon Days",
+        artist: "Ellie Goulding",
+      },
+    ],
+  }),
 };
 </script>
