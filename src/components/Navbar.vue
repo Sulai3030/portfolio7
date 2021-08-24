10fc4acf24a8218971785fd5cdedd99f6ba6dfcc
sssulai@gmail.com <you@example.com>
2021-08-24 15:02:54 -0400

    changed color of nav font

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,30 @@
 <template>
   <nav>
-    <v-toolbar app color="#022770">
-      <v-toolbar-title class="uppercase">
+    <v-toolbar app color="#022770" prominent class="font-weight-white">
+      <v-toolbar-title class="white--text">
         Sulai Sivadel
         <span>Web Developer</span>
+        <v-spacer></v-spacer> <v-spacer></v-spacer>
+        <v-btn class="white--text" elevation="24" outlined raised rounded
+          ><span>About</span></v-btn
+        >
+        <v-btn class="white--text" elevation="24" outlined raised rounded
+          ><span>Applications</span></v-btn
+        >
+        <v-btn class="white--text" elevation="24" outlined raised rounded
+          ><span>Work Experince</span></v-btn
+        >
+        <v-btn class="white--text" elevation="24" outlined raised rounded
+          ><span>Writing Experince</span></v-btn
+        >
+        <v-btn class="white--text" elevation="24" outlined raised rounded
+          ><span>Skills</span></v-btn
+        >
       </v-toolbar-title>
     </v-toolbar>
   </nav>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    items: [
-      {
-        color: "##4680F2",
-        src: "",
-        title: "",
-        artist: "",
-      },
-      {
-        color: "##F2755E",
-        src: "",
-        title: "",
-        artist: "",
-      },
-    ],
-  }),
-};
+export default {};
 </script>
