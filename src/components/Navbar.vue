02a76bdd5e8960a1a7a55cd3fed2ee1ff45ec5e3
sssulai@gmail.com <you@example.com>
2021-08-31 13:13:35 -0400

    deleted old app bar added nav drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,65 @@
 <template>
-  <v-app-bar app color="#022770" prominent class="font-weight-white">
-    <v-toolbar-title class="white--text">
-      Sulai Sivadel
-      <v-spacer></v-spacer>Web Developer</v-toolbar-title
-    ><v-spacer></v-spacer> <v-spacer></v-spacer>
-    <v-btn class="white--text" elevation="24" outlined raised rounded to="/"
-      ><span>About</span></v-btn
-    >
-    <v-btn
-      class="white--text"
-      elevation="24"
-      outlined
-      raised
-      rounded
-      to="/applications"
-      ><span>Applications</span></v-btn
-    >
+  <v-card class="mx-auto overflow-hidden" height="150" width="2000">
+    <v-app-bar color="#022770" dark class="white--text">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Sulai Sivadel : Web Developer</v-toolbar-title>
+    </v-app-bar>
 
-    <v-btn
-      class="white--text"
-      elevation="24"
-      outlined
-      raised
-      rounded
-      to="/Clients"
-      ><span>Clients</span></v-btn
-    >
-    <v-btn
-      class="white--text"
-      elevation="24"
-      outlined
-      raised
-      rounded
-      to="/WritingExperience"
-      ><span>Professional Writing</span></v-btn
-    >
-    <v-btn
-      class="white--text"
-      elevation="24"
-      outlined
-      raised
-      rounded
-      to="/Skills"
-      ><span>Skills</span></v-btn
-    >
-  </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="#022770--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Applications</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Clients</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Writing Experience</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Skills</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
+
+<style></style>
