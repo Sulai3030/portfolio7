97a329abf3c48aeffb3d23ca498a23ef2774de97
sssulai@gmail.com <you@example.com>
2021-08-31 19:47:20 -0400

    deleted old app bar added nav drawer changed color but drawer won't work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,49 +5,65 @@
       <v-toolbar-title>Sulai Sivadel : Web Developer</v-toolbar-title>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="#022770--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Applications</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Clients</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Writing Experience</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Skills</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      absolute
+      temporary
+      height="100%"
+      color="#4680F2"
+      class="white--text"
+    >
+      <v-list nav class="white--text">
+        <v-list-tile
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+          ><v-list-item-group
+            v-model="group"
+            active-class="#022770--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Applications</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="white--text">Clients</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="white--text"
+                >Writing Experience</v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="white--text">Skills</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="white--text">Contact</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
   </v-card>
@@ -55,10 +71,22 @@
 
 <script>
 export default {
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "Home", text: "Home", route: "/" },
+        { icon: "folder", text: "Applications", route: "/applications" },
+        { icon: "person", text: "Clients", route: "/clients" },
+        {
+          icon: "person",
+          text: "WritingExperience",
+          route: "/writingexperience",
+        },
+        { icon: "person", text: "Skills", route: "/skills" },
+      ],
+    };
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import MenuIcon from "vue-material-design-icons/Menu.vue";
 
 Vue.config.productionTip = false;
+
+Vue.component("menu-icon", MenuIcon);
 
 new Vue({
   router,
