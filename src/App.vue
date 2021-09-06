5ebccc0009950475486c6d08f6c7515376462730
sssulai@gmail.com <you@example.com>
2021-09-06 14:49:34 -0400

    added boilerplate to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-container
-    ><v-row
-      ><v-column
-        ><v-app app dark>
-          <v-card
-            ><Navbar />
+  <!-- App.vue -->
+  <v-app>
+    <v-app-bar app> <!-- --><Navbar /> </v-app-bar>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-row
+        ><v-column>
+          <v-card>
             <v-btn
               v-for="link in links"
               :key="`${link.label}-footer-link`"
@@ -20,10 +23,17 @@
               tile
               >{{ link.label }}</v-btn
             >
-            <router-view></router-view
-          ></v-card>
-          <Footer /> </v-app></v-column></v-row
-  ></v-container>
+          </v-card>
+        </v-column></v-row
+      >
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app> <!-- --><Footer /> </v-footer>
+  </v-app>
 </template>
 <script>
 import Navbar from "@/components/Navbar";
